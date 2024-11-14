8aca1eab256d60cccdeafe50c37e3d6bcca1087e
Mahelchandupa <mahelchandupa1013@gmail.com>
2024-11-14 23:00:05 +0530

    install tailwind css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-       <h1>
+       <h1 className=" text-blue-500 text-center text-2xl">
          Bus Ticket Booking System
        </h1>
     </div>
